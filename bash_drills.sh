9029e4270e021ac3b2fe654b608913df93834897
Daniel Muyshond <daniel.muyshond@gmail.com>
2019-08-20 10:02:32 +0200

    updating bash_drills

--- bash_drills.sh
+++ bash_drills.sh
@@ -1,40 +1,23 @@
 #!/bin/sh
-
 echo "Hello World"
-
 # paramètres et variables
   #  le répertoire depuis on exécute ${PWD}
 echo "La commande st exécutée depuis : ${PWD}"
   #  la localisation du fichier script
 echo "Le script est localisé là-bas : ${BASEDIR}"
-
-
-## Je l'écris comme je le pense avant de réspecter la syntaxe
-#
-# Switch 
-#   case cdusr
-#   case cdopt
-#   case sass
-#       si style.scss existe, 
-#         sass style.scss style.css
-#       else
-#         echo 'pas de style.scss dans ' 
-#   case restore
-#       # $2 = paramètre 2 est sensé être une ville donc on vérifie si le dossier est bien existant
-#       si -d /opt/publik/scripts/imio-publik-themes/static/$2 
-#         cp /opt/publik/scripts/imio-publik-themes/static/$2 /usr/share/publik/themes/imio/static
-#       else
-#         echo 'pas de $2 dans le répertoire static (/opt/publik/scripts/imio-publik-themes/static/)'
-#   case save
-#       #check faute de frappe
-#       si -d /usr/share/publik/themes/imio/static/$2
-#         cp /usr/share/publik/themes/imio/static/$2 /opt/publik/scripts/imio-publik-themes/static/$2
-#   default 
-#     echo
-#     Faciliter l\'utilisation de sass
-#       /n cdusr='cd /usr/share/publik/themes/imio/static' 
-#       /n cdopt='cd /opt/publik/scripts/imio-publik-themes/static/'
-#       /n sass='sass style.scss style.css'
-#
-## idées d'amelioration
-## utiliser rsync au lieu de tout cp pour save
+# functions
+  # déclarer la function
+Hello() {
+  echo "Hello iMio"
+}
+  # l'utiliser
+Hello
+# rendre le script executable
+  # chmod +x bash_drills.sh
+# executer le script
+  # ./bash_drilles.sh
+# actual terminal output
+  # Hello World
+  # La commande st exécutée depuis : /home/daniel/bash-a-sable
+  # Le script est localisé là-bas : 
+  # Hello iMio
--- ts_tool.sh
+++ ts_tool.sh
@@ -1,11 +1,10 @@
+#essai 1
 #Je laisse tomber ceci et repars de zéro en privilégiant l'utilisant d'un switch
-
 # #!/bin/bash
 # function ts_tool() {
 # # feedbacks strings (DRY)
 # err="> Erreur : "
 # ok="> Succès : "
-
 # # si il n'y a pas 2 parametres
 # if [[ $# != 2 ]]; then
 # 	# installer sass ?
@@ -44,3 +43,36 @@
 # fi
 # }
 
+
+
+## essai 2
+## Je l'écris comme je le pense
+# Switch 
+#   case cdusr
+#   case cdopt
+#   case sass
+#       si style.scss existe, 
+#         sass style.scss style.css
+#       else
+#         echo 'pas de style.scss dans ' 
+#   case restore
+#       # $2 = paramètre 2 est sensé être une ville donc on vérifie si le dossier est bien existant
+#       si -d /opt/publik/scripts/imio-publik-themes/static/$2 
+#         cp /opt/publik/scripts/imio-publik-themes/static/$2 /usr/share/publik/themes/imio/static
+#       else
+#         echo 'pas de $2 dans le répertoire static (/opt/publik/scripts/imio-publik-themes/static/)'
+#   case save
+#       #check faute de frappe
+#       si -d /usr/share/publik/themes/imio/static/$2
+#         cp /usr/share/publik/themes/imio/static/$2 /opt/publik/scripts/imio-publik-themes/static/$2
+#   default 
+#     echo
+#     Faciliter l\'utilisation de sass
+#       /n cdusr='cd /usr/share/publik/themes/imio/static' 
+#       /n cdopt='cd /opt/publik/scripts/imio-publik-themes/static/'
+#       /n sass='sass style.scss style.css'
+#
+## idées d'amelioration
+## utiliser rsync au lieu de tout cp pour save
+
+ts_tool()
