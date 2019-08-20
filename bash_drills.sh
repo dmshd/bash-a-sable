08cb08e798ef41ab809f7dd999ae0dd74138d1dc
Daniel Muyshond <daniel.muyshond@gmail.com>
2019-08-20 11:45:41 +0200

    ts_tool wip

--- bash_drills.sh
+++ bash_drills.sh
@@ -1,17 +1,40 @@
 #!/bin/sh
-echo "Hello World"
+separatorContent="\n(⌒▽⌒) - - - - - -  (⌒▽⌒)\n"
+echo "🔥 Hello World 👋"
+separator() {
+  echo $separatorContent
+}
+
 # paramètres et variables
   #  le répertoire depuis on exécute ${PWD}
-echo "La commande st exécutée depuis : ${PWD}"
+echo "🔥 La commande st exécutée depuis : ${PWD}"
   #  la localisation du fichier script
-echo "Le script est localisé là-bas : ${BASEDIR}"
+echo "🔥 Le script est localisé là-bas : ${BASEDIR}"
+separator
+echo "🔥 Paramètres passés (jusqu'à 9 possibles)"
+echo '🔥 $0 = ' $0
+if [ "$1" != "" ]
+then
+  echo "🔥 Super tu as entré un paramètre ✅"
+else
+  echo "❌  Tu dois entrer 1 paramètre."
+fi
+echo '🔥 $1 = ' $1
+# echo '$2 = ' $2
+# echo '$3 = ' $3
+# ...
+separator
+
+  # Détecter le nombre d'argumaents
+
 # functions
   # déclarer la function
 Hello() {
-  echo "Hello iMio"
+  echo "🔥 Hello iMio 👋"
 }
   # l'utiliser
 Hello
+separator
 # rendre le script executable
   # chmod +x bash_drills.sh
 # executer le script
@@ -21,3 +44,34 @@ Hello
   # La commande st exécutée depuis : /home/daniel/bash-a-sable
   # Le script est localisé là-bas : 
   # Hello iMio
+
+
+# options de ligne de commande quand on execute le script
+# msgs retour utilisateur
+msgOutil="🛠️  Outil local_téléservice >"
+er="❌ Erreur :"
+ok="✅ Succès :"
+
+# check if arg != 0
+check_arg() {
+  if [ "$2" = "" ]; then
+        echo "$msgOutil $er Il manque un argument."
+  fi
+}
+
+if [ "$1" != "" ]; then
+  case $1 in 
+    -s | --save )
+      check_arg
+      break
+      ;;
+    -r | --restore )
+      check_arg
+    * )
+      echo "$msgOutil $er Il manque une option ou l'option n'est pas reconnue. \n   Les options sont : \n      🔥  --save \n      🔥  --restore \n      🔥  --sass"
+      break
+      ;;
+  esac
+else 
+ echo "$msgOutil $er L'option n'est pas reconnue. \n   Les options sont : \n      🔥  --save \n      🔥  --restore \n      🔥  --sass"
+fi
